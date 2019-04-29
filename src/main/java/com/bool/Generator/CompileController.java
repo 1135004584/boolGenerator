984e5360954889dd09564b13fe19eb735aaf466a
bool <1135004584@qq.com>
2019-04-29 16:56:10 +0800

    2019-4-29

--- src/main/java/com/bool/Generator/CompileController.java
+++ src/main/java/com/bool/Generator/CompileController.java
@@ -23,14 +23,58 @@ public class CompileController {
      * @return
      */
     public CompileModel Compile(String name,List<Row> rowsList){
+        CompileModel compileModel = new CompileModel();
         Row row;
+        StringBuffer ret = new StringBuffer();
         for(int i=0;i<rowsList.size();i++)
         {
             row = (Row)rowsList.get(i);
-
-
+            if("row".equals(row.getName()))//当元素类型为row
+            {
+                ret.append(getRetByRow(row));
+            }else if("append".equals(row.getName()))//当元素类型为append
+            {
+                ret.append(getRetByAppend(row));
+            }else if("var".equals(row.getName()))//当元素类型为var
+            {
+                ret.append(getRetByVar(row));
+            }else if("if".equals(row.getName()))//当元素类型为if
+            {
+                ret.append(getRetByIf(row));
+            }else if("for".equals(row.getName()))//当元素类型为for
+            {
+                ret.append(getRetByFor(row));
+            }
         }
 
+        compileModel.fileName(name+getSuffixByOutput(name));
+        compileModel.setContent(ret.toString());
+
+        return compileModel;
+    }
+
+    private String getSuffixByOutput(String name) {
+        
+        return null;
+    }
+
+    private String getRetByFor(Row row) {
+        return null;
+    }
+
+    private String getRetByIf(Row row) {
+        return null;
+    }
+
+    private String getRetByVar(Row row) {
+        return null;
+    }
+
+    private String getRetByAppend(Row row) {
+        return null;
+    }
+
+    private String getRetByRow(Row row) {
         return null;
     }
 }
